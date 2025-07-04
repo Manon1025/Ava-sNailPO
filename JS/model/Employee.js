534f422d8b657685965acb020c540bc39b276d87
Manon1025 <manon.bl.dev@gmail.com>
2025-07-04 12:35:36 +0200

    ajout dossier model + fichier collection: categroy, employee, documents

---
+++ JS/model/Employee.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const EmployeeSchema = new mongoose.Schema ({
+    _id: {type : mongoose.Schema.ObjectId, auto: true}, //est généré par mongoDB
+    fname: String,
+    lname: String,
+    avatar: String,
+    birth_date: Date,
+    email: String,
+    password: String,
+    adresse: {
+        adress: String,
+        cp: Number,
+        city: String,
+    },
+    documents: {
+        url: String,
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: 'user'
+    },
+    isActive: Boolean,
+    created_at: Date,
+})
+module.exports = mongoose.model('Employee', EmployeeSchema)
