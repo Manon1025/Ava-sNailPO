0fdc70f88f1c2ea357325455a8a086a4a8260008
Manon1025 <manon.bl.dev@gmail.com>
2025-07-04 16:51:33 +0200

    ajout le delete

--- JS/app.js
+++ JS/app.js
@@ -12,12 +12,40 @@ const connexionDB = require('./database')
 connexionDB()
 
 // ! lancement serv
+    // * Visu sur toute la liste des employées
 app.get('/employee', async(req, res) => {
     try {
         const employees = await Employee.find()
         res.status(200).json({employees})
     } catch (err) {
         throw err 
+    }
+})
+
+    // * Visu sur un des employées
+app.get('/employee/:id', async(req,res) => {
+    try {
+        const id = req.params['id']
+        const oneEmployee = await Employee.findById(id)
+        res.status(200).send(`
+            <h1> ${oneEmployee.fname} ${oneEmployee.lname} </h1>
+            `)
+    } catch (err) {
+        throw err
+    }
+})
+
+    // * Pouvoir supprimer un employee
+app.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params['id']
+        let result = await Employee.findByIdAndDelete(id)
+        // if (!result) {
+        //     return res.status(404).send('Employé non trouvé')
+        // }
+        res.status(200).json({ message: `employé ${result.fname} ${result.lname} supprimé` })
+    } catch (err) {
+        res.status(500).json({ error: err.message })
     }
 })
 
