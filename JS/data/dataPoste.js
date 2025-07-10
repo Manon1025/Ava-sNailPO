c278032b8f9a7459bc5721bd38461bd6c29aeba5
Manon1025 <manon.bl.dev@gmail.com>
2025-07-10 16:25:24 +0200

    ajout dans la base de donnée mongo les postes

---
+++ JS/data/dataPoste.js
@@ -0,0 +1,8 @@
+const postes = [
+  { name: "Salarie" },
+  { name: "Stagiaire" },
+  { name: "Apprenti" },
+  { name: "Interim" },
+];
+
+module.exports = postes
--- JS/data/database.js
+++ JS/data/database.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
-// const Category = require('../model/Category')
-// const data = require('./dataCategory')
+const Poste = require('../model/Poste')
+const data = require('./dataPoste')
 
 module.exports = async () => {
     try {
         await mongoose.connect('mongodb://localhost:27017/Projet')
         console.log('connexion réussie !')
 
-        // await Category.insertMany(data)
+        // await Poste.insertMany(data)
         // console.log('Salariés insérés !')
     } catch (err) {
         throw err
