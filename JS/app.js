d47400218694d208d1a06ade3ebcde7625e741ac
Manon1025 <manon.bl.dev@gmail.com>
2025-08-25 16:49:26 +0200

    modification remise a zero de la session pour me connecter

--- JS/app.js
+++ JS/app.js
@@ -6,9 +6,8 @@ const path = require('path');                                               // ?
 const expressLayouts = require('express-ejs-layouts')                       // ? permet de gérer les layouts EJS
 const cookieParser = require('cookie-parser')                               // ? permet de parser les cookies
 const session = require('express-session')                                  // ? permet de gérer les sessions
-const MongoStore = require('connect-mongo');
-
-require('dotenv').config();
+const MongoStore = require('connect-mongo')
+require('dotenv').config()
 
     // * FICHIER
 const routerEmployee = require('./routes/routeEmployee')
@@ -34,16 +33,25 @@ app.use(session({
     secret: process.env.SESSION_SECRET,                             // ? clé secrète pour signer les cookies de session
     resave: false,
     saveUninitialized: true,
-    store: MongoStore.create({                                               // ? à mettre à true si tu utilises HTTPS
-        mongoUrl : process.env.DataMongoURL,
-        ttl: 14 * 24 * 60 * 60
-        }),
-    cookie: {
-        secure: process.env.NODE_ENV === 'production',                      // ? True si HTTPS
-        httpOnly: true,
-        maxAge: 14 * 34 * 60 * 60 * 1000                                    // ? 14 jours en ms
-    }
+    cookie: { secure: false }                                               // ? à mettre à true si tu utilises HTTPS
 }))
+
+// app.set('trust proxy', 1); // nécessaire pour HTTPS derrière un proxy
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,                             // ? clé secrète pour signer les cookies de session
+//     resave: false,
+//     saveUninitialized: false,
+//     store: MongoStore.create({                                               // ? à mettre à true si tu utilises HTTPS
+//         mongoUrl : process.env.DataMongoURL,
+//         ttl: 14 * 24 * 60 * 60
+//         }),
+//     cookie: {
+//         secure: process.env.NODE_ENV === 'production',                      // ? True si HTTPS
+//         httpOnly: true,
+//         maxAge: 14 * 34 * 60 * 60 * 1000                                    // ? 14 jours en ms
+//     }
+// }))
+
 
 app.use((req, res, next) => {
     res.locals.user = req.session.user || null;
@@ -60,7 +68,6 @@ app.use('/', isAuth ,routerPages)
 app.use('/', isAuth ,routerDocument)
 app.use('/', isAuth ,routerEmployee)
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log('Server running on port ${PORT}')
+app.listen(1024, () => {
+    console.log('Connection server Success')
 })
